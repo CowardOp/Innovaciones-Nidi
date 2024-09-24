93778d13bfe3d17b46564b9427791867a22f5a40
Brayan Casallas <beto980730@gmail.com>
2024-09-24 12:07:43 -0500

    Act

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,17 +1,6 @@
-import Aside from "../Aside/index.";
-import "./main.css";
 const Main = () => {
-  const list = ["Corte", "Peinados", "Color", "Manicure"];
-  const classList = [
-    "https://cdn-icons-png.flaticon.com/512/32/32069.png",
-    "https://cdn-icons-png.flaticon.com/512/40/40575.png",
-    "https://cdn-icons-png.flaticon.com/512/83/83027.png",
-    "https://cdn-icons-png.flaticon.com/512/3461/3461937.png",
-  ];
   return (
     <>
-      <Aside data={list} clase={classList} />
-
       <div className="h-screen"></div>
     </>
   );
